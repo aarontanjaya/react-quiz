17f744e85d0c2e60aaf13b34192e3de8ab510737
Aaron Tanjaya <aaront.tjy@gmail.com>
2023-10-30 15:07:42 +0700

    feat: initial Test1 answer, add input output

--- src/routes/Test1/index.js
+++ src/routes/Test1/index.js
@@ -1,13 +1,18 @@
+import { useState } from "react";
+
 const Test1 = () => {
+  const [value, setValue] = useState("")
+  const formatInput = (input) =>(input ? input:"__________")
+
   return(
     <div>
       <ul>
         <li>Please render the inputted value</li>
       </ul>
       <p>
-        <b>User Input: </b> ______
+        <b>User Input: </b> <p>{formatInput(value)}</p>
       </p>
-      <input type="text" placeholder="input here"/>
+      <input onChange={(e)=>setValue(e.target.value)} type="text" placeholder="input here"/>
     </div>
   )
 }
