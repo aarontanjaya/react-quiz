4ee69ddba947fcab4bb310fa4779394cbfe0d32a
Aaron Tanjaya <aaront.tjy@gmail.com>
2023-10-31 11:18:46 +0700

    feat: initial answer for test6, add pause, mute, and skip ahead/backwards functionality

--- src/routes/Test6/Player.js
+++ src/routes/Test6/Player.js
@@ -1,16 +1,61 @@
-import { cssVideo } from "./style";
+import { useEffect, useRef, useState } from "react";
+import { cssContainer, cssVideo } from "./style";
 
 const Player = () => {
+  const [paused, setPaused] = useState(true)
+  const [muted, setMuted] = useState(false)
+  const [time, setTime] = useState(0)
+  const [duration, setDuration] = useState(0)
+  const vidRef = useRef(0)
+  const handleTogglePlay = (paused)=>{
+    if(paused){
+      vidRef.current.play()
+      return
+    }
+    vidRef.current.pause()
+  }
+
+  const handleToggleMute = ()=>{
+    setMuted(!muted)
+  }
+
+  const formatTime = (s)=>{
+    const mins = Math.floor(s/60)
+    const secs = Math.ceil(s%60)
+    return `${mins < 10? `0${mins}`:mins}:${secs < 10 ? `0${secs}`:secs}`
+  }
+
+  useEffect(()=>{
+    if (vidRef){
+      setPaused(vidRef.current.paused)
+      vidRef.current.muted = muted
+    }
+  }, [vidRef.current.paused, muted])
+
   return(
-    <div>
+    <div className={cssContainer}>
       <video
+        onPause={()=>{
+          setPaused(true)
+        }}
+        onPlay={()=>{
+          setPaused(false)
+        }}
+        onTimeUpdate={()=>{
+          setTime(vidRef.current.currentTime)
+        }}
+        onLoadedMetadata={()=>{
+          setDuration(vidRef.current.duration)
+        }}
+        ref={vidRef}
         className={cssVideo}
         src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
       />
-      <button type="button">Play/Pause</button>
-      <button type="button">Mute/Unmute</button>
-      <input type="range"/>
-      <span>00:00/00:00</span>
+      <button onClick={()=> handleTogglePlay(vidRef.current.paused)} type="button">{paused? "Play":"Pause"}</button>
+      <button onClick={()=>handleToggleMute()} type="button">{muted ? "Unmute": "Mute"}</button>
+      <input value={time} onChange={(e)=>{
+        vidRef.current.currentTime = e.target.value}} max={vidRef.current.duration} min={0} type="range"/>
+      <span>{`${formatTime(time)}/${formatTime(duration)}`}</span>
     </div>
   )
 }
--- src/routes/Test6/style.js
+++ src/routes/Test6/style.js
@@ -3,7 +3,10 @@ import { css } from 'react-emotion';
 export const cssVideo = css({
   display: 'block',
   width: "100%",
-  height: 600,
+  height: "75%",
   objectFit: 'cover',
   marginTop: 16,
 });
+
+export const cssContainer = css({
+})
